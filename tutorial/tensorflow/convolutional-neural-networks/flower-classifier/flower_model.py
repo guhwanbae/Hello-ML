08a0decd2ee0239e2ee3c5646ebbbde09fc68758
Gu-hwan Bae <guhwan.bae@gmail.com>
2019-08-27 00:03:28 +0900

    Image augmentation for FlowerModel dataset
      Randomly apply flipp, crop and affine transform which includes
      rotation and scailing to make agumented images.

--- tutorial/tensorflow/convolutional-neural-networks/flower-classifier/flower_model.py
+++ tutorial/tensorflow/convolutional-neural-networks/flower-classifier/flower_model.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.image as mpimg
 from pathlib import Path
 from six.moves import urllib
+import cv2
 
 class FlowerModel:
     def __init__(self, path=None):
@@ -18,6 +19,7 @@ class FlowerModel:
         self.labels = None
         self.class_to_image_paths = None
         self.class_to_images = None
+        self.class_and_path_pairs = None
     
     def get_class_labels(self):
         if self.labels is not None:
@@ -29,6 +31,12 @@ class FlowerModel:
                 labels.append(flower_class)
         self.labels = sorted(labels)
         return self.labels
+    
+    def class_to_idx(self):
+        return {flower_class: idx for idx, flower_class in enumerate(self.get_class_labels())}
+    
+    def idx_to_class(self):
+        return {idx: flower_class for idx, flower_class in enumerate(self.get_class_labels())}
     
     def _extract_jpg_image_paths(self, image_dir_path):
         file_paths = []
@@ -51,7 +59,16 @@ class FlowerModel:
             if image_paths is not None:
                 self.class_to_image_paths[flower_class] = np.asarray(image_paths)
         return self.class_to_image_paths  
-
+    
+    def get_class_and_path_pairs(self):
+        if self.class_and_path_pairs is not None:
+            return self.class_and_path_pairs
+        
+        self.class_and_path_pairs = []
+        for flower_class, paths in self.get_class_to_image_paths().items():
+            self.class_and_path_pairs += [(flower_class, path) for path in paths]
+        return self.class_and_path_pairs
+    
     def download(self):
         if os.path.exist(self.path):
             return
@@ -81,7 +98,28 @@ class FlowerModel:
             self.class_to_images[flower_class] = np.array([mpimg.imread(fname) for fname in image_paths])
         return self.class_to_images  
     
-    def get_random_samples(self, flower_class, n_samples):
+    def get_random_samples(self, flower_class, n_samples, resize_shape=None):
         n_images = len(self.class_to_image_paths[flower_class])
         rand_idx = np.random.choice(n_images-1, n_samples)
-        return [mpimg.imread(fname) for fname in self.class_to_image_paths[flower_class][rand_idx]]
+        samples = [mpimg.imread(fname) for fname in self.class_to_image_paths[flower_class][rand_idx]]
+        if resize_shape is not None:
+            samples = [cv2.resize(sample, dsize=resize_shape) for sample in samples]
+        return samples
+    
+    def random_batch(self, batch_size, augmentation_op=None, size=None):
+        class_and_path_pairs = np.asarray(self.get_class_and_path_pairs())
+        class_to_idx = self.class_to_idx()
+        n_samples = len(class_and_path_pairs)
+        n_batches = int(np.ceil(n_samples // batch_size))
+        random_idx = np.random.permutation(n_samples)
+        for idx in np.array_split(random_idx, n_batches):
+            X_batch = np.array([mpimg.imread(path) for flower_class, path in class_and_path_pairs[idx]])
+            y_batch = np.array([class_to_idx[flower_class] for flower_class, path in class_and_path_pairs[idx]])
+            if augmentation_op:
+                X_batch = np.repeat(X_batch, size)
+                y_batch = np.repeat(y_batch, size)
+                X_batch = np.array([augmentation_op(image) for image in X_batch])
+                shuffle_idx = np.random.permutation(len(X_batch))
+                X_batch = X_batch[shuffle_idx]
+                y_batch = y_batch[shuffle_idx]
+            yield X_batch, y_batch
---
+++ tutorial/tensorflow/convolutional-neural-networks/flower-classifier/image_transform.py
@@ -0,0 +1,40 @@
+import numpy as np
+import skimage
+
+def random_crop(image, max_margin):
+    height, width, n_channels = image.shape
+    x_start = np.random.randint(0, int(height * max_margin))
+    x_end = np.random.randint(int(height * (1-max_margin)), height)
+    y_start = np.random.randint(0, int(width * max_margin))
+    y_end = np.random.randint(int(width * (1-max_margin)), width)
+    return image[x_start:x_end, y_start:y_end, :]
+
+def random_flip(image, vertical_flip, horizontal_flip):
+    if np.random.rand() < horizontal_flip:
+        image = image[:,::-1,:]
+    if np.random.rand() < vertical_flip:
+        iamge = image[::-1,:,:]
+    return image
+
+def random_affine(image, max_zoom, max_angle_rad, max_shear, max_shift):
+    rand_zoom = np.random.uniform(1-max_zoom, 1+max_zoom, 2)
+    rand_angle_rad = np.random.uniform(-max_angle_rad, max_angle_rad)
+    rand_shear = np.random.uniform(-max_shear, max_shear)
+    height, width, n_channels = image.shape
+    rand_shift = [np.random.uniform(-height*max_shift, height*max_shift),
+                  np.random.uniform(-width*max_shift, width*max_shift)]
+    T = skimage.transform.AffineTransform(scale=rand_zoom,
+                                          shear=rand_shear,
+                                          rotation=rand_angle_rad,
+                                          translation=rand_shift)
+    return skimage.transform.warp(image, T.inverse, mode='reflect')
+
+def random_transform(image, shape=(299,299),
+                     max_zoom=0.2, max_angle_rad=0.3,
+                     max_shear=0.2, max_shift=0.1,
+                     vertical_flip=0.3, horizontal_flip=0.3,
+                     max_margin=0.1):
+    output = random_affine(image, max_zoom, max_angle_rad, max_shear, max_shift)
+    output = random_flip(output, vertical_flip, horizontal_flip)
+    output = random_crop(output, max_margin)
+    return skimage.transform.resize(output, shape)
